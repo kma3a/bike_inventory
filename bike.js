9f42e7317ed2ece5bfb897801c553141fbb8b8f4
Kelsey Ando <kma3a@virginia.edu>
2015-03-19 10:10:27 -0400

    adding function to change the saleprice of the bike

--- bike.js
+++ bike.js
@@ -5,3 +5,11 @@ function Bike(brand, model, color, salePrice) {
 	this.color = color;
 	this.salePrice = salePrice;
 }
+
+Bike.prototype.updatePrice = function(newPrice) {
+	this.salePrice = newPrice;
+	console.log("The price of the " +this.color + " " + this.brand + " " + this.model + " is now " + this.salePrice + ".");
+}
+
+myBike = new Bike("harley", "Zoom", "Yellow", 123.20);
+myBike.updatePrice(103.20);
